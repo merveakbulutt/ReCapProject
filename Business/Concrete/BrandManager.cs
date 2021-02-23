39931dc115dcee1b039ee4b7cfa8795c7057a17d
Katip <Katip@DESKTOP-6P2FP43>
2021-02-23 13:08:54 +0300

    Autofac,fluent validation and AOP added.

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -17,17 +19,14 @@ namespace Business.Concrete
         {
             _brandDal = brandDal;
         }
+
+        [ValidationAspect(typeof(BrandValidator))]
         public IResult Add(Brand brand)
         {
 
-            if (brand.BrandName.Length > 2)
-            {
-               
-                return new SuccessResult(Messages.BrandAdded);
-            }
             _brandDal.Add(brand);
 
-            return new ErrorResult(Messages.BrandInvalid);
+            return new SuccessResult(Messages.BrandAdded);
         }
 
         public IResult Delete(Brand brand)
--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -18,17 +20,14 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+
+        [ValidationAspect(typeof(CarValidator))]
         public  IResult Add(Car car)
         {
 
-            if (car.DailyPrice > 0)
-            {
-                return new SuccessResult(Messages.CarAdded);
-
-            }
             _carDal.Add(car);
            
-            return new ErrorResult(Messages.CarDailyPrice);
+            return new SuccessResult(Messages.CarAdded);
         }
 
         public IResult Delete(Car car)
